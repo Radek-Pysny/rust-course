61cff2b04e4dc56d7f088674e0a8765481bf0aa4
Radek Pyšný <radek.pysny@oddin.gg>
2023-11-27 20:08:02 +0100

    Used `bail!` macro + fixed timestamp format string.

--- lecture-2023-10-31-xchat/client/src/lib.rs
+++ lecture-2023-10-31-xchat/client/src/lib.rs
@@ -13,7 +13,7 @@ use std::time;
 use color_eyre::eyre;
 #[cfg(not(debug_assertions))]
 use ::anyhow as eyre;
-use eyre::{anyhow, Result, Context};
+use eyre::{bail, Result, Context};
 
 use commands::{Command, MessageType};
 use shared::{Message, panic_to_text};
@@ -36,10 +36,10 @@ pub fn run_interactive(address: &str) -> Result<()> {
 
     let mut stream = match TcpStream::connect(address) {
         Ok(stream) => stream,
-        Err(err) => Err(format!("failed to connect: {}", err.to_string()))?,
+        Err(err) => bail!("failed to connect: {}", err.to_string()),
     };
     if let Err(err) = stream.set_nonblocking(true) {
-        Err(format!("failed to set stream attribute: {}", err.to_string()))?;
+        bail!("failed to set stream attribute: {}", err.to_string());
     }
 
     // Channel for sending of commands from input thread to processing thread.
@@ -201,10 +201,9 @@ pub fn run_interactive(address: &str) -> Result<()> {
 
 
 /// `join_thread` is just a helper function converting possible errors from thread panicking.
-fn join_thread<T: Display>(handle: JoinHandle<T>) -> Result<()> {
-    match handle.join() {
-        Ok(ok) => {},
-        Err(err) => anyhow!("thread panicked: {}", panic_to_text(err)),
+fn join_thread<T>(handle: JoinHandle<T>) -> Result<()> {
+    if let Err(err) = handle.join() {
+        bail!("thread panicked: {}", panic_to_text(err))
     };
     Ok(())
 }
@@ -216,7 +215,7 @@ fn save_image(payload: Vec<u8>) -> Result<()> {
     use std::time::SystemTime;
 
     let now: DateTime<Local> = SystemTime::now().into();
-    let timestamp = now.format("%Y-%m-%dT%H:%I");
+    let timestamp = now.format("%Y-%m-%dT%H:%M:%S");
 
     let filepath_str = format!("./images/{}.png", timestamp);
     let filepath = Path::new(filepath_str.as_str());
@@ -240,18 +239,18 @@ fn save_file(filename: &String, payload: Vec<u8>) -> Result<()> {
 fn _save_file(filepath: &Path, content: Vec<u8>) -> Result<()> {
     // create needed directories on path to the target file (if needed)
     if let Err(err) = create_dir_all(filepath.parent().unwrap()) {
-        anyhow!("failed to prepare directories: {}", err.to_string());
+        bail!("failed to prepare directories: {}", err.to_string());
     }
 
     // create a new file (possibly truncating any already existing)
     let mut f = match File::create(&filepath) {
         Ok(file) => file,
-        Err(err) => anyhow!("failed to create file: {}", err.to_string()),
+        Err(err) => bail!("failed to create file: {}", err.to_string()),
     };
 
     // write all the binary data into an empty file open for writing
     match f.write_all(&content) {
         Ok(_) => Ok(()),
-        Err(err) => anyhow!("failed to write into file: {}", err.to_string()),
+        Err(err) => bail!("failed to write into file: {}", err.to_string()),
     }
 }
