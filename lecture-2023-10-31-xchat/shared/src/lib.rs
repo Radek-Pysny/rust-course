85d88a432e126bd9604ae5cf0603f632a3e51b88
Radek Pyšný <radek.pysny@oddin.gg>
2023-12-10 19:55:07 +0100

    Implementing page for the list of users.

--- lecture-2023-10-31-xchat/shared/src/lib.rs
+++ lecture-2023-10-31-xchat/shared/src/lib.rs
@@ -5,3 +5,22 @@ mod timestamp;
 pub use message::Message;
 pub use panic::panic_to_text;
 pub use timestamp::timestamp_to_string;
+
+
+pub fn concat(strings: &Vec<String>) -> String {
+    use std::borrow::{Borrow};
+
+    if strings.is_empty() {
+        return String::new();
+    }
+
+    // `len` calculation may overflow but push_str will check boundaries
+    let len = strings.iter().map(|s| <String as Borrow<String>>::borrow(s).len()).sum();
+    let mut result = String::with_capacity(len);
+
+    for s in strings {
+        result.push_str(s.borrow())
+    }
+
+    result
+}
